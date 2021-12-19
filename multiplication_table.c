d6fe6977a5177789d6a7e755360119c221abff7b
Nikesh_Bedari <59239254+Nikesh-Bedari@users.noreply.github.com>
2021-12-19 13:54:36 +0545

    Update and rename small.c to multiplication_table.c

--- multiplication_table.c
+++ multiplication_table.c
@@ -3,8 +3,12 @@
 
 void main(){
     int i,j;
+    
+    //asking user for inputs
     printf("Enter your number:\n");
     scanf("%d", &i);
+    
+    //the main logic is used for for loop
     for (j=1;j<=10;++j) {
         printf("%d * %d = %d\n",i,j,i*j);
     }
